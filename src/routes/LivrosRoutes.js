e81532bfbbb7b00ea38a07490588e61017c7d24c
Eduardo Morais <dudu.vgvm@gmail.com>
2023-12-22 12:29:36 -0300

    Projeto Completo

--- src/routes/LivrosRoutes.js
+++ src/routes/LivrosRoutes.js
@@ -3,6 +3,7 @@ import LivroController from "../controller/LivroController.js";
 
 const routes = express.Router();
 routes.get("/livros", LivroController.listarLivros);
+routes.get("/livros/busca", LivroController.listarPorEditora);
 routes.get("/livros/:id", LivroController.listarLivroPorId);
 routes.post("/livros", LivroController.cadastrarLivro);
 routes.put("/livros/:id", LivroController.atualizarLivro);
